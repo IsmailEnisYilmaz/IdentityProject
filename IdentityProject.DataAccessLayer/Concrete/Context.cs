fc2c12d3e7f9506529af22b591e0be58713bf762
Enis <yilmaz.ismailenis@gmail.com>
2023-08-06 16:33:21 +0300

    tema ve identity eklenmesi

---
+++ IdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using IdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-B033PI3\\SQLEXPRESS; initial catalog = IdentityProjectDb; integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
